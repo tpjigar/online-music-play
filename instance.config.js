76290e5b33423da31b369a80d99cbedb6a8e2e56
JIGAR <tp.jigar@gmail.com>
2020-04-22 18:50:00 +0530

    Package install

---
+++ instance.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  keyColorBG: '#5339F1',
+  keyColorFG: '#FFFFFF',
+  secondColorBG: '#1A1A1A',
+  secondColorFG: '#FFFFFF',
+  loadingColor: '#FFCC00',
+  slug: process.env.COSMIC_BUCKET || 'onlinemusicplay',
+  read_key: process.env.COSMIC_READ_KEY || '1lDSfzQKlCKIUP7LX6wPLFU0JGgeSsIajizsiHKbVK2Sdbc9Z4',
+  write_key: process.env.COSMIC_WRITE_KEY || '8cwBdhuW8y9btiX2nHUxWOMDoGJc3tUvyJgTC4CD944aKidjRJ'
+}
